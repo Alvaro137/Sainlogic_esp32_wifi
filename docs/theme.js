aeb73630418f3f8b3b93d26f284b215741d35536
Alvaro137 <Alvagilsan@gmail.com>
2025-07-30 15:23:33 +0200

    Reemplazo versión web

--- docs/theme.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("darkModeToggle").addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    const isDarkMode = document.body.classList.contains("dark-mode");
-    document.getElementById("darkModeToggle").textContent = isDarkMode ? "🌙" : "☀️";
-});
